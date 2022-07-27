dd5b25987cbf30d798067b5f3dc25de0b24b4995
João Fabris <jvfabris21@gmail.com>
2022-07-26 22:11:31 -0300

    Req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './Utils/renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('2. Teste o componente <About.js />.', () => {
+  it('Testa se a página contém um heading h2 com o texto de erro', () => {
+    renderWithRouter(<NotFound />);
+    const textPage = screen.getByText('Page requested not found');
+    expect(textPage).toBeInTheDocument();
+  });
+  it('Teste se a página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(src);
+  });
+});
